d0dbb5fb8642a4e5c5dad7e67174a4e9645acb9f
Swangxiaorong <Swangxiaorong@aliyun.com>
2021-05-07 16:48:02 +0800

    链接基本完成

---
+++ wwwroot/js/zhaoping.js
@@ -0,0 +1,336 @@
+$(function () {
+    //单次单选弹框
+    $("#onlyChoseAlert1").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert2").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert3").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert4").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert5").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert6").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert7").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert8").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert9").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlert10").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //
+    //单次单选弹框
+    $("#onlyChoseAlertt1").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt2").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt3").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt4").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt5").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt6").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt7").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt8").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt9").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlertt10").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //
+    //单次单选弹框
+    $("#onlyChoseAlerttt1").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt2").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt3").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt4").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt5").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt6").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt7").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt8").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt9").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //单次单选弹框
+    $("#onlyChoseAlerttt10").click(function () {
+        var onlyChoseAlert = simpleAlert({
+            "content":"简历请投递以下邮箱<br />长沙 pengda@diamsaw.com.cn<br /> 株洲 zzdl@diamsaw.com",
+            "buttons":{
+                "确定":function () {
+                    onlyChoseAlert.close();
+                }
+            }
+        })
+    })
+    //
+})
+
